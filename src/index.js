ca03a5f295516d335a270ea734cde6092b0ed12f
gevelynanzelbi-cell <g.evelyn.anzelbi@gmail.com>
2026-02-25 04:55:27 +0000

    PWL_Pertemuan4

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 function hello(nama, umur) {
-    return 'Selamat Datang ' + nama + ' di PWL, umur ' + nama + ' adalah';
+    return 'Selamat Datang ' + nama + ' di PWL, umur ' + umur + ' adalah';
 }
 const mahasiswa = "Andi";
 const umur = 20;
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 function hello(nama: string, umur: number): string {
-    return 'Selamat Datang '+nama+' di PWL, umur '+nama+' adalah';
+    return 'Selamat Datang '+nama+' di PWL, umur '+umur+' adalah';
 }
 const mahasiswa = "Andi";
-const umur = 20;
+const umur:number = 20;
 console.log(hello(mahasiswa,umur));
